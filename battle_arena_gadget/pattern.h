786ff203fe6958689247264337a1b6e320ddae03
dannyc-dev <onlinesuport.team@gmail.com>
2022-01-07 16:27:46 -0500

    rename

--- battle_arena_gadget/pattern.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include <TlHelp32.h>
-
-char* PatternScanModule(HANDLE hProcess, uintptr_t begin, uintptr_t end, const char* pattern, const char* mask);
-char* PatternScan(const char* pattern, const char* mask, char* begin, intptr_t size);
